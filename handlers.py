3b8820faa8ce156853dacfc8d8dec757b5ab752b
v1zKO <102490515+v1zKO@users.noreply.github.com>
2022-03-27 23:14:13 -0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -11,9 +11,7 @@ def start(update, context):
 
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
-        text=f"""
-📞 Connected {user_info}.
-        """,
+        pass
     )
 
 
